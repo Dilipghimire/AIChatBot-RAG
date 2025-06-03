3cdbff07950e03e610bc1dc2276b2c1dcb9a09fb
Dilip Ghimire <Dilipghimire2@gmail.com>
2025-06-03 12:01:09 -0400

    loading icon on login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,60 +2,68 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "./login.scss";
 import { userLogin } from "../hooks/user-login";
+import Loading from "../components/ui/loading";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
   const { mutateAsync } = userLogin();
 
   const handleLogin = async () => {
     try {
+      setIsLoading(true);
       await mutateAsync({ username, password });
       navigate("/landing");
       localStorage.setItem("isLoggedIn", "true");
-
     } catch (err) {
+      setIsLoading(false);
       setError("Invalid username or password");
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="login-page">
-      <div className="login-box">
-        <h1 className="app-title">AI Video Assistant</h1>
-        <p className="app-description">
-          This app transcribes and summarizes YouTube videos, and lets you ask
-          questions about their content. Gain quick insights and better
-          understanding of complex topics.
-        </p>
-        <input
-          className="username-input"
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          onFocus={() => setError("")}
-        />
-        <input
-          className="password-input"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          onFocus={() => setError("")}
-        />
-        <button className="login-btn" onClick={handleLogin}>
-          Log In
-        </button>
-        {error?.length ? <span className="login-error">{error}</span> : null}
-        <Link to="/register" className="register-link">
-          Don't have an account? Register here
-        </Link>
+    <>
+      {isLoading && <Loading />}
+      <div className="login-page">
+        <div className="login-box">
+          <h1 className="app-title">AI Video Assistant</h1>
+          <p className="app-description">
+            This app transcribes and summarizes YouTube videos, and lets you ask
+            questions about their content. Gain quick insights and better
+            understanding of complex topics.
+          </p>
+          <input
+            className="username-input"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onFocus={() => setError("")}
+          />
+          <input
+            className="password-input"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onFocus={() => setError("")}
+          />
+          <button className="login-btn" onClick={handleLogin}>
+            Log In
+          </button>
+          {error?.length ? <span className="login-error">{error}</span> : null}
+          <Link to="/register" className="register-link">
+            Don't have an account? Register here
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
